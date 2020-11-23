62f32b0b2d3789997d053193076134101de80487
Rafael Scheidt <rfscheidt@gmail.com>
2020-11-23 11:19:36 -0300

    comentario na classe calculadora

--- src/br/com/aula2/Calculadora.java
+++ src/br/com/aula2/Calculadora.java
@@ -2,6 +2,9 @@ package br.com.aula2;
 
 public class Calculadora {
 	
+	/*
+	 * teste
+	 */
 	public static double calcularMedia(double a, double b, double c) {
 		double media = (a + b + c) / 3;
 		
